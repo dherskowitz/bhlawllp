ddf328f83ac7306a9aa7726c9aafce1bab0ceec6
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2022-09-02 15:27:26 -0400

    responsive styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import compress from "astro-compress";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress()]
+  integrations: [tailwind(), image(), compress(), alpinejs()]
 });
