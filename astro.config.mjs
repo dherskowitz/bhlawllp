3e8fbbb379e01777c39234613ff61a7728b0b242
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2023-06-27 13:53:53 -0400

    upgrade astro, add content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import compress from "astro-compress";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress(), alpinejs()]
+  integrations: [tailwind(), alpinejs()]
 });
