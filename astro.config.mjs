2d38a90079c166db20a7243392e8e351340e27b7
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2025-03-25 19:33:01 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs()]
 });
---
+++ src/services.js
@@ -0,0 +1,22 @@
+export const services = [
+    {
+      title: "Residential Real Estate",
+      content:
+        "Our experienced team will guide you through your real estate closings, ensuring all contracts are negotiated efficiently and all files close in a seamless and timely manner. We have particular experience including purchases and sales, covering joint ownership arrangements, condominiums, cooperatives and tenancies-in-common and like-kind 1031 exchanges.",
+    },
+    {
+      title: "Commercial Real Estate",
+      content:
+        "Our firm handles all aspects of commercial real estate transactions, from letters of intent through closing and post-closing matters. We specialize in smaller family-owned and startup real estate businesses.",
+    },
+    {
+      title: "Estate Planning",
+      content:
+        "Let us help you protect your assets, plan for your future, and decide how they should be distributed to your heirs through proper estate planning. Our firm can advise and assist you with setting up trusts and handling other estate planning needs. From how state and federal laws can affect your plans, to advising you on estate planning strategies, our firm can explore what options are available that best fit your goals.",
+    },
+    {
+      title: "Real Estate Financing",
+      content:
+        "Breuer Herskowitz specializes in the representation of both lenders and borrowers in various loan transactions. Our attorneys have broad experience handling closings on behalf of lending institutions in New York, working with various parties to ensure smooth and efficient transactions. In addition to representing institutional and lenders, we represent our real estate clients in a wide variety of personal financing transactions.",
+    },
+  ];
